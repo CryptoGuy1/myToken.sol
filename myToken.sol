c7c19aa138f1c705ce067c62042883c5ebbb2eec
CryptoGuy1 <benblockdev@gmail.com>
2025-04-17 21:40:11 -0600

    mint function

--- myToken.sol
+++ myToken.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts@4.6.0/token/ERC20/ERC20.sol";
@@ -8,16 +7,16 @@ import "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
 contract Token is ERC20, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC20("Chainlink Bootcamp 2024 Token", "CLBoot24") {}
-
-    // Grant the deployer admin and minter roles
-    function grantAdminAndMinter(address _admin) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        _grantRole(DEFAULT_ADMIN_ROLE, _admin);
-        _grantRole(MINTER_ROLE, _admin);
+    constructor() ERC20("Chainlink Bootcamp 2024 Token", "CLBoot24") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    // Allow a user to approve another as a minter role programmatically
-    function approveAsMinter(address _userToApprove, address _approver) external onlyRole(DEFAULT_ADMIN_ROLE) {
-        grantRole(MINTER_ROLE, _userToApprove);
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 2;
     }
 }
